663c5c8520fc2ca52683cd7e39f93d018ee634a9
lubiano83 <lubiano83@gmail.com>
2023-12-01 10:14:06 -0300

    agregando mas cosas

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* 1era Entrega */
 
 const CURSOS = {
-    46915: { cantidadAlumnos: 150, claseIniciada: true },
-    49810: { cantidadAlumnos: 86, claseIniciada: true },
-    49575: { cantidadAlumnos: 94, claseIniciada: true },
-    53210: { cantidadAlumnos: 57, claseIniciada: false },
-    54060: { cantidadAlumnos: 42, claseIniciada: false },
-    57245: { cantidadAlumnos: 21, claseIniciada: false },
-    57120: { cantidadAlumnos: 36, claseIniciada: false },
+    46915: { cantidadAlumnos: 150, claseIniciada: true, claseFinalizado: true, top10: true },
+    49810: { cantidadAlumnos: 86, claseIniciada: true, claseFinalizado: false, top10: false },
+    49575: { cantidadAlumnos: 94, claseIniciada: true, claseFinalizado: false, top10: false },
+    53210: { cantidadAlumnos: 57, claseIniciada: false, claseFinalizado: false, top10: false },
+    54060: { cantidadAlumnos: 42, claseIniciada: false, claseFinalizado: false, top10: false },
+    57245: { cantidadAlumnos: 21, claseIniciada: false, claseFinalizado: false, top10: false },
+    57120: { cantidadAlumnos: 36, claseIniciada: false, claseFinalizado: false, top10: false },
 };
 
 function ingreseComision() {
@@ -17,11 +17,19 @@ function ingreseComision() {
         numeroComision = parseInt(prompt("Número de comisión no disponible. Ingrese otro número:"));
     }
 
-    if (CURSOS[numeroComision].claseIniciada || CURSOS[numeroComision].cantidadAlumnos === 150) {
+    if (CURSOS[numeroComision].claseFinalizado && CURSOS[numeroComision].top10 == false) {
+        alert ("Felicitaciones, Curso Terminado!!");
+    }
+
+    else if (CURSOS[numeroComision].claseFinalizado && CURSOS[numeroComision].top10) {
+        alert ("Felicitaciones, terminaste top10!!");
+    }
+
+    else if (CURSOS[numeroComision].claseIniciada || CURSOS[numeroComision].cantidadAlumnos === 150) {
         alert ("El curso ya inicio o alcanzo el maximo de alumnos");
     
     } else {
-        alert ("El curso aun esta disponible");
+        alert ("El curso aun no comienza");
     }
 } ingreseComision();
 
