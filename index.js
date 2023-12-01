3e06215e1b4c7eac002e7640b8fc8fe50938dbf3
lubiano83 <lubiano83@gmail.com>
2023-12-01 10:00:59 -0300

    ajuste cursos

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
 /* 1era Entrega */
 
-let cursos = {
-
-    46915: {
-        cantidadAlumnos: 150,
-        claseIniciada: true,
-    },
-    49810: {
-        cantidadAlumnos: 86,
-        claseIniciada: true,
-    },
-    49575: {
-        cantidadAlumnos: 94,
-        claseIniciada: true,
-    },
-    53210: {
-        cantidadAlumnos: 57,
-        claseIniciada: false,
-    },
-    54060: {
-        cantidadAlumnos: 42,
-        claseIniciada: false,
-    },
-    57245: {
-        cantidadAlumnos: 21,
-        claseIniciada: false,
-    },
-    57120: {
-        cantidadAlumnos: 36,
-        claseIniciada: false,
-    },
-
-}
+const cursos = {
+    46915: { cantidadAlumnos: 150, claseIniciada: true },
+    49810: { cantidadAlumnos: 86, claseIniciada: true },
+    49575: { cantidadAlumnos: 94, claseIniciada: true },
+    53210: { cantidadAlumnos: 57, claseIniciada: false },
+    54060: { cantidadAlumnos: 42, claseIniciada: false },
+    57245: { cantidadAlumnos: 21, claseIniciada: false },
+    57120: { cantidadAlumnos: 36, claseIniciada: false },
+};
 
 function ingreseComision() {
     let numeroComision = parseInt(prompt("Ingrese número de comisión sin el #"));
