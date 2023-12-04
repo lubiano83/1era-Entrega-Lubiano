8d1c65d1ef560100f4a2a975b0e513bdb4039863
lubiano83 <lubiano83@gmail.com>
2023-12-04 13:55:43 -0300

    ajustes varios

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* 1era Entrega */
 
 const CURSOS = {
-    46915: { cantidadAlumnos: 150, claseIniciada: true, claseFinalizado: true, top10: true },
-    49810: { cantidadAlumnos: 86, claseIniciada: true, claseFinalizado: false, top10: false },
-    61560: { cantidadAlumnos: 86, claseIniciada: true, claseFinalizado: false, top10: false },
-    49575: { cantidadAlumnos: 94, claseIniciada: true, claseFinalizado: false, top10: false },
-    53210: { cantidadAlumnos: 57, claseIniciada: false, claseFinalizado: false, top10: false },
-    54060: { cantidadAlumnos: 42, claseIniciada: false, claseFinalizado: false, top10: false },
-    57245: { cantidadAlumnos: 21, claseIniciada: false, claseFinalizado: false, top10: false },
-    57120: { cantidadAlumnos: 36, claseIniciada: false, claseFinalizado: false, top10: false },
-    59520: { cantidadAlumnos: 43, claseIniciada: false, claseFinalizado: false, top10: false },
+    46915: { cantidadAlumnos: 150, claseIniciada: true, claseFinalizada: true, top10: true },
+    49810: { cantidadAlumnos: 86, claseIniciada: true, claseFinalizada: false, top10: false },
+    61560: { cantidadAlumnos: 86, claseIniciada: true, claseFinalizada: false, top10: false },
+    49575: { cantidadAlumnos: 94, claseIniciada: true, claseFinalizada: false, top10: false },
+    53210: { cantidadAlumnos: 57, claseIniciada: false, claseFinalizada: false, top10: false },
+    54060: { cantidadAlumnos: 42, claseIniciada: false, claseFinalizada: false, top10: false },
+    57245: { cantidadAlumnos: 21, claseIniciada: false, claseFinalizada: false, top10: false },
+    57120: { cantidadAlumnos: 36, claseIniciada: false, claseFinalizada: false, top10: false },
+    59520: { cantidadAlumnos: 43, claseIniciada: false, claseFinalizada: false, top10: false },
 };
 
 function ingreseComision() {
@@ -24,10 +24,10 @@ function ingreseComision() {
             alert ("Ese numero de Comision no existe");
             return ingreseComision ();
         }
-        if (CURSOS[numeroComision].claseFinalizado && CURSOS[numeroComision].top10 == false) {
+        if (CURSOS[numeroComision].claseFinalizada && CURSOS[numeroComision].top10 == false) {
             alert ("Felicitaciones, Curso Terminado!!");
         }
-        else if (CURSOS[numeroComision].claseFinalizado && CURSOS[numeroComision].top10) {
+        else if (CURSOS[numeroComision].claseFinalizada && CURSOS[numeroComision].top10) {
             alert ("Felicitaciones, Terminaste Top10!!");
         }
         else if (CURSOS[numeroComision].claseIniciada && CURSOS[numeroComision].cantidadAlumnos === 150) {
